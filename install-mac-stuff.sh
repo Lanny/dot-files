05439fa31f6c3bf2d36eed80c1a786605ea6c5b9
RyanJenkins <rjenkins@mail.sfsu.edu>
2018-09-03 15:12:52 -0700

    Add homebrew installation to setup.

--- install-mac-stuff.sh
+++ install-mac-stuff.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# Install homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Install zsh
 brew install zsh
 curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh
 chsh -s "$(which zsh)"
