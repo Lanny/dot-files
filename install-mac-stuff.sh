119d3669416fe76f534185a20d9f9402710b8b65
RyanJenkins <rjenkins@mail.sfsu.edu>
2018-09-03 17:22:29 -0700

    Link up qutebrowser config as part of install.

--- install-mac-stuff.sh
+++ install-mac-stuff.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# Link up qutebrowser config
+mkdir -p ~/Library/Preferences/qutebrowser
+ln qutebrowser/autoconfig.yml ~/Library/Preferences/qutebrowser
+
 # Install homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
